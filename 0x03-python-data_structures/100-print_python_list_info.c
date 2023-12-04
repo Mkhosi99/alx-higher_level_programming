7683b9e462a24e9f21abadc730e43f71366e7a4c
Mkhosi99 <mkhosishabalala2@gmail.com>
2023-12-04 16:21:12 +0200

    A C function that will print some basic info about Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,30 +1,30 @@
 #include "Python.h"
 
 /**
- * display_python_list_info - Prints information about python objects
- * @python_list: PyObject pointer to print info about
+ * print_python_list_info - Prints information about python objects
+ * @p: PyObject pointer to print info about
  * Compile with:
  * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
  * -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
  * 100-print_python_list_info.c
 */
 
-void display_python_list_info(PyObject *python_list)
+void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t index, list_size;
 	PyObject *list_item;
 	const char *element_type;
 	PyListObject *list_object;
 
-	list_object = (PyListObject *)python_list;
-	list_size = PyList_Size(python_list);
+	list_object = (PyListObject *)p;
+	list_size = PyList_Size(p);
 
 	printf("[*] Size of the Python List = %d\n", (int)list_size);
 	printf("[*] Allocated = %d\n", (int)list_object->allocated);
 
 	for (index = 0; index < list_size; index++)
 	{
-		list_item = PyList_GetItem(python_list, index);
+		list_item = PyList_GetItem(p, index);
 		element_type = Py_TYPE(list_item)->tp_name;
 		printf("Element %d: %s\n", (int)index, element_type);
 	}
